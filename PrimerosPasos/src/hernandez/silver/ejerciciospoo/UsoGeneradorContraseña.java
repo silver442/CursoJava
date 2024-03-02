76eff671da767cca478a901b7c6acc7af89f3838
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-03-01 23:23:18 -0600

    uso de la herencia

---
+++ PrimerosPasos/src/hernandez/silver/ejerciciospoo/UsoGeneradorContraseña.java
@@ -0,0 +1,10 @@
+package hernandez.silver.ejerciciospoo;
+
+public class UsoGeneradorContraseña {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- PrimerosPasos/src/hernandez/silvestre/poo/UsoEmpleados.java
+++ PrimerosPasos/src/hernandez/silvestre/poo/UsoEmpleados.java
@@ -1,52 +1,86 @@
 package hernandez.silvestre.poo;
 
+import java.util.GregorianCalendar;
+
 public class UsoEmpleados {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Empleados empleado1=new Empleados("Anna");
+/*
+		Empleados Antonio= new Empleados("Antonio", 2300.5, 2005, 7, 15);
 		
-		Empleados empleado2=new Empleados("Antonio");
+		Jefes Ana=new Jefes("Ana", 2900, 2008, 8, 9);
 		
-		Empleados empleado3=new Empleados("Maria");
+		Ana.setIncentivo(300.5);
 		
-		System.out.println(empleado1.getDatosEmpleado());
+		System.out.println(Ana.getSueldo());*/
+	
+		Jefes Juan =new Jefes("Juan", 8000, 2013,8,5);
 		
-		empleado1.setSeccion("RRHH");
+		Juan.setIncentivo(200);
 		
-		System.out.println(empleado1.getDatosEmpleado());
+		Empleados[] losEmpleados=new Empleados[6];
 		
-		System.out.println(empleado2.getDatosEmpleado());
+		losEmpleados[0]=new Empleados("Anotonio", 2300.5, 2005, 7, 5);
+		losEmpleados[1]=new Empleados("Carlos", 500.5, 2007, 6, 5);
+		losEmpleados[2]=new Empleados("María", 2500.5, 2006, 11, 7);
+		losEmpleados[3]=new Empleados("Ana", 7000, 2009, 5, 3);
+		losEmpleados[4]=Juan; // Principio de sustitucion
+		losEmpleados[5]=new Jefes("Isabel",8000,2007,4,2);
 		
-		System.out.println(empleado3.getDatosEmpleado());
+		Jefes Isabel=(Jefes)losEmpleados[5];
 		
-		System.out.println(Empleados.getIdSiguiente());
+		Isabel.setIncentivo(500);
+		/*Empleados Patricia=new Jefes("Patricia", 2500,2007,5,6);
+		
+		Jefes PatriciaDirector=(Jefes)Patricia;*/
+		
+		for(Empleados obj:losEmpleados) {
+			
+			System.out.println(obj.getDatosEmpleado() + " y un salario de " + obj.getSueldo());
+			
+		}	
 	}
 }
 
 class Empleados{
 	
-	public Empleados(String nombre) {
+	public Empleados(String nom, double sue, int agno, int mes, int dia) {
 		
-		this.nombre=nombre;
+		nombre=nom;
 		
-		seccion="Administración";
+		sueldo=sue;
+		
+		calendario=new GregorianCalendar(agno, mes, dia);
 		
 		Id=IdSiguiente;
 		
 		IdSiguiente++;
 	}
 	
-	public void setSeccion(String seccion) {
+	public String getDatosEmpleado() {
 		
-		this.seccion=seccion;
+		return "El empleado " + nombre + " tiene el Id "+Id;
+	}
+	
+	public double getSueldo() {
+		
+		return sueldo;
 		
 	}
 	
-	public String getDatosEmpleado() {
+	public GregorianCalendar getFechaAlta() {
 		
-		return "El empleado " + nombre + " pertenece a la sección de " + seccion+" y tienen el n° "+Id;
+		return calendario;
+		
+	}
+	
+	public void setSubeSueldo(double porcentaje) {
+		
+		double aumento=sueldo*porcentaje/100;
+		
+		sueldo+=aumento;
+		
 	}
 	
 	public static String getIdSiguiente() {
@@ -56,10 +90,37 @@ class Empleados{
 	}
 	
 	private final String nombre;
+
+	private double sueldo;
 	
-	private String seccion;
+	GregorianCalendar calendario;
 	
 	private int Id;
 	
 	private static int IdSiguiente=1;
+}
+
+class Jefes extends Empleados{
+
+	public Jefes(String nom, double sue, int agno, int mes, int dia) {
+		super(nom, sue, agno, mes, dia);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setIncentivo(double b) {
+		
+		incentivo=b;
+		
+	}
+	
+	public double getSueldo() { // sobre escribe el metodo getSueldo de la clase padre
+		
+		double sueldoJefe=super.getSueldo(); //con super llama a la clase padre
+		
+		return sueldoJefe + incentivo;
+		
+	}
+	
+	private double incentivo;
+	
 }
