c6e8e7b8efc5965ba7ab09578671e129e1c2bfc0
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-04-18 12:02:23 -0600

    creacion de componentes en la interfaz, labels, textFields, checks y radio

--- JavaFx3/src/aplicacionCompleta/PedidosPizza.java
+++ JavaFx3/src/aplicacionCompleta/PedidosPizza.java
@@ -2,6 +2,7 @@ package aplicacionCompleta;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.text.*;
@@ -71,8 +72,82 @@ public class PedidosPizza extends Application{
 		
 		VBox paneCliente=new VBox(10, paneNombre, paneTfno, paneDireccion);
 		
+		//-------Creación del pane de los pedidos-----------
 		
+		// Creación del pane del tamaño de las pizzas
 		
+		Label lblTamano=new Label("Tamaño");
+		
+		radioPeq=new RadioButton("Pequeña");
+		
+		radioMed=new RadioButton("Mediana");
+		
+		radioGran=new RadioButton("Grande");
+		
+		radioMed.setSelected(true);
+		
+		ToggleGroup grupoTamano=new ToggleGroup();
+		
+		radioPeq.setToggleGroup(grupoTamano);
+		
+		radioMed.setToggleGroup(grupoTamano);
+		
+		radioGran.setToggleGroup(grupoTamano);
+		
+		VBox paneTamano=new VBox(lblTamano, radioPeq, radioMed, radioGran);
+		
+		paneTamano.setSpacing(10);
+		
+		// Creación del pane de la masa
+
+		Label lblMasa=new Label("Masa");
+		
+		radioFina=new RadioButton("Fina");
+		
+		radioNormal=new RadioButton("Normal");
+
+		// Seleccionado por defecto
+		radioFina.setSelected(true);
+		
+		ToggleGroup grupoMasa=new ToggleGroup();
+		
+		radioFina.setToggleGroup(grupoMasa);
+		
+		radioNormal.setToggleGroup(grupoMasa);
+		
+		VBox paneMasa=new VBox(lblMasa, radioFina, radioNormal);
+		
+		paneMasa.setSpacing(10);
+		
+		// Creación del pane Ingredientes
+		
+		Label lblIngredientes=new Label("Ingredientes");
+		
+		chkPepperoni=new CheckBox("Peperoni");
+		
+		chkQueso=new CheckBox("Queso");
+		
+		chkPimiento=new CheckBox("Pimiento");
+		
+		chkAceitunas=new CheckBox("Aceitunas");
+		
+		chkCampi=new CheckBox("Champiñon");
+		
+		chkTomate=new CheckBox("Tomate");
+		
+		chkAnchoas=new CheckBox("Anchoas");
+		
+		FlowPane paneIngredientes=new FlowPane(Orientation.VERTICAL,chkPepperoni,chkQueso,chkPimiento,chkAceitunas,chkCampi,chkTomate,chkAnchoas );
+		
+		paneIngredientes.setPadding(new Insets(10,0,10,0));
+		
+		paneIngredientes.setHgap(20);
+		
+		paneIngredientes.setVgap(10);
+		
+		paneIngredientes.setPrefWrapLength(100);
+		
+		VBox paneIngredientesVertical=new VBox(lblIngredientes,paneIngredientes);
 	}
 	
 	public void main(String[] args) {
