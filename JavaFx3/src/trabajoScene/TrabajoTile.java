e3da1d25fab816cea6d623d87d2a242b47907488
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-04-22 15:52:46 -0600

    trabajando con mas layout, TillPane

---
+++ JavaFx3/src/trabajoScene/TrabajoTile.java
@@ -0,0 +1,58 @@
+package trabajoScene;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class TrabajoTile extends Application{
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+		TilePane miPane=new TilePane();
+
+		// establecer separacion horizontal
+		miPane.setHgap(10);
+		
+		miPane.setVgap(10);
+		
+		miPane.setPadding(new Insets(10,10,10,10));
+		
+		for(int i=1;i<13;i++) {
+			
+			Rectangle r=new Rectangle(100, 100);
+			
+			r.setFill(Color.rgb(125, 240, 215));
+			
+			Label etiq=new Label("Cuadrado " + i);
+			
+			StackPane miStack=new StackPane(r, etiq);
+			
+			miPane.getChildren().add(miStack);
+		}
+		
+		
+		Scene laScene=new Scene(miPane, 800,600);
+		
+		primaryStage.setScene(laScene);
+		
+		primaryStage.show();
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+
+		launch(args);
+	}
+
+	
+
+}
