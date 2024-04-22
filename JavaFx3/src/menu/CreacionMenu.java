d6965bed56399e11fcfbcc991566e72452b3ceaa
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-04-22 14:59:38 -0600

    trabajando con checkmenu item y radioMenuitem con Javafx

--- JavaFx3/src/menu/CreacionMenu.java
+++ JavaFx3/src/menu/CreacionMenu.java
@@ -30,6 +30,10 @@ public class CreacionMenu extends Application{
 		
 		menuBar.getMenus().add(MenuInsertar);
 		
+		Menu menuOpciones=new Menu("Opciones");
+		
+		menuBar.getMenus().add(menuOpciones);
+		
 		//-----------Menu Items---------------
 		
 		menuItemNuevo=new MenuItem("Nuevo");
@@ -64,6 +68,36 @@ public class CreacionMenu extends Application{
 		
 		menuItemPegar.setDisable(true);
 		
+		RadioMenuItem menuItemOp1=new RadioMenuItem("Opción 1");
+		
+		RadioMenuItem menuItemOp2=new RadioMenuItem("Opción 2");
+		
+		RadioMenuItem menuItemOp3=new RadioMenuItem("Opción 3");
+		
+		RadioMenuItem menuItemOp4=new RadioMenuItem("Opción 4");
+		
+		ToggleGroup opciones=new ToggleGroup();
+		
+		menuItemOp1.setToggleGroup(opciones);
+		
+		menuItemOp2.setToggleGroup(opciones);
+		
+		menuItemOp3.setToggleGroup(opciones);
+		
+		menuItemOp4.setToggleGroup(opciones);
+		
+		
+		menuItemOp1.setOnAction(e->accionMenus(e));
+		
+		menuItemOp2.setOnAction(e->accionMenus(e));
+		
+		menuItemOp3.setOnAction(e->accionMenus(e));
+		
+		menuItemOp4.setOnAction(e->accionMenus(e));
+		
+		
+		menuOpciones.getItems().addAll(menuItemOp1, menuItemOp2, menuItemOp3, menuItemOp4);
+		
 		menuEdicion.getItems().add(menuItemCortar);
 		
 		menuEdicion.getItems().add(menuItemCopiar);
@@ -95,13 +129,24 @@ public class CreacionMenu extends Application{
 		
 		MenuItem item=(MenuItem)e.getSource();
 		
-		System.out.println("Has pulsado en el menú de: " + item.getText());
+		//System.out.println("Has pulsado en el menú de: " + item.getText());
 	
 		//if(item.getText().equals("Guardar como")) item.setText("Guardar");
 	
 		if(item.getText().equals("Guardar como")) menuItemGuardar.setDisable(true);
 	
-		if(item.getText().equals("Cortar")) menuItemPegar.setDisable(false);
+		if(item.getText().equals("Cortar")) menuItemPegar.setDisable(false  );
+		
+		if(item.getText().equals("Copiar")) menuItemPegar.setDisable(false  );
+		
+		/*if(((RadioMenuItem) item).isSelected()==true) System.out.println(item.getText() + " activada");
+		else System.out.println(item.getText() + " desactivada");*/
+		
+		if(e.getSource().getClass().getSimpleName().equals("RadioMenuItem")) {
+			
+			System.out.println(item.getText() + " activada");
+			
+		}
 		
 	}
 	
